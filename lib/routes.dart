eb52332c4eeb54440b38298e0df4f3b1bf5062ca
Messaoud Hamdi <89160125+hmdmsd@users.noreply.github.com>
2023-01-15 21:02:09 +0100

    Delete routes.dart

--- lib/routes.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-final Map<String, WidgetBuilder> Routes = {
-  //'/login': (context) => Login_Screen(),
-  //'/signup1': (context) => Signup_Screen1(),
-};
